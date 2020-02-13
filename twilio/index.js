6e707bfabe3604146f67e1f4e8d90527aaf54fde
Tahshara Boochee <boocheet@gmail.com>
2020-02-12 20:00:11 -0800

    added ngrok to twilio server

--- twilio/index.js
+++ twilio/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('Hello world this is Tahshara!'));
 
 app.listen(port, () => console.log('example app listening on port: ' + port));
