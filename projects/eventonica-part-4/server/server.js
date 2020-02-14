6be2ce0bd76cf511882e4166d7669150079af453
Tahshara Boochee <boocheet@gmail.com>
2020-02-14 15:59:31 -0800

    completed the api and it is working

--- projects/eventonica-part-4/server/server.js
+++ projects/eventonica-part-4/server/server.js
@@ -1,9 +1,14 @@
+const app = document.getElementById('root')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+app.appendChild(container)
+
 // creates a request variable and assign a new XMLHttpReqest object to it
 let request = new XMLHttpRequest();
-//creates a global Javascript variable
+//creates a global Javascript variables keyword and category to search for events
 let keyword;
 let category;
-// console.log(keyword)
+
 
 
 document.getElementById('keyword-button').onclick = searchClick;
@@ -12,7 +17,7 @@ document.getElementById('category-search').onclick = searchClick;
 //when user enters keyword a request will be sent to ticketmaster's API
 function searchClick(e){
   e.preventDefault();
-  document.getElementById('ticketmaster-events').innerHTML= '';
+  document.getElementById('root').innerHTML= '';
   keyword = document.getElementById('keyword-search-value').value
   // category = document.getElementById('category-search-id').value
   
@@ -28,29 +33,29 @@ function searchClick(e){
   request.open("GET", `http://app.ticketmaster.com/discovery/v1/events.json?keyword=${keyword}&source=universe&countryCode=US&apikey=i3iTEyBGu6DSxPykBtaEllniCz2OLxOj`, true)
   request.onload = function(){
     //accessing events through JSON data
-    console.log('inside of the search click response:', this.response)
     let data = JSON.parse(this.response);
     data = data._embedded.events
-    console.log('inside of the search click data:', data)
+    // console.log('inside of the search click data:', data)
     let category = document.getElementById("category-search")
     if (request.status >= 200 && request.status < 400) {
-      console.log('inside of the search click:', data)
+      // console.log('inside of the search click:', data)
       data.forEach(event => {
         console.log('inside of the json data obj:', event)
         const card = document.createElement('div')
         card.setAttribute('class', 'card')
-        const ul = document.getElementById('ticketmaster-events');
-        const ul1 = document.getElementById('all-events');
-        const h1 = document.createElement('h1')
-        h1.textContent = event.name
+        //if have a ticket-master card section
+        // const ul = document.getElementById('ticketmaster-events');
+        const ul = document.getElementById('all-events');
+        const h3 = document.createElement('h3')
+        h3.textContent = event.name
   
         const p = document.createElement('p')
-        // event.description = event.description.substring(0, 300)
-        p.textContent = `${event.description}...`
+        p.textContent = event.dates;
+        p.textContent = event.description
   
-        // container.appendChild(card)
-        ul.appendChild(h1)
-        ul.appendChild(p)
+        container.appendChild(card)
+        app.appendChild(h3)
+        app.appendChild(p)
       })
     } else {
       const errorMessage = document.createElement('marquee')
